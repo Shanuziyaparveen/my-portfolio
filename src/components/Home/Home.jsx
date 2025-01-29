308aea2d932043d5b9c13e1b84df4f6dc560c6c2
shanuziya <shanuziyacp@gmail.com>
2025-01-29 15:06:49 +0530

    Initial commit with portfolio

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
                 <h1 className='text-3xl md:text-6xl font-poppins flex leading-normal tracking-tighter'>Hello, I am Shanuziya Parveen</h1>
                 <p className='text-sm md:text-2xl tracking-tight'>A highly motivated and results-driven Web Developer with expertise in the MERN stack,
                 seeking opportunities as a Full Stack Developer, Frontend Developer, or Backend Developer to utilize my skills in building efficient, scalable, and user-centric applicatio</p>
-                <br>
-                </br>
+               
+                <br/>
                 <a
                     href="mailto:shanuziyacp@gmail.com?subject=Contact%20Request&body=Hello,%20I%20would%20like%20to%20get%20in%20touch."
                     className='mt-5 md:md-10 py-2 px-3 text-sm md:text-xl md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#05a3ae]'
